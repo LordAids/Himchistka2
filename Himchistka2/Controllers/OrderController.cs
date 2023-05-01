623d3f41367e8aaab8398d59c43608109c0cde35
KKokin <ktakida@bk.ru>
2023-05-01 19:28:25 +0300

    Шаблоны контроллера для работы с заказом

---
+++ Himchistka2/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Himchistka.Api.Controllers
+{
+    [Route("api/Orders")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet("{orderId}")]
+        public IActionResult GetOrderById([FromRoute] Guid orderId)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetOrders()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult UpsertOrder()
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{orderId}")]
+        public IActionResult DeleteOrder([FromRoute] Guid orderId) 
+        {
+            return Ok();
+        }
+    }
+}
