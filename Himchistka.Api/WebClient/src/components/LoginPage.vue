02787b57b550a360b1ef7d15703e25458a1c71a0
KKokin <ktakida@bk.ru>
2023-06-08 00:17:47 +0300

    веб клиент init

---
+++ Himchistka.Api/WebClient/src/components/LoginPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class=" align-center text-center">
+      <h1>Авторизация</h1>
+      
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+  //
+</script>
